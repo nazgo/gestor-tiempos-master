3a9c2d7e4a454b093231d55e6f303cad750563c6
nazgo <37985499+nazgo@users.noreply.github.com>
2026-07-20 17:00:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -752,8 +752,26 @@ def nueva_competencia():
                 '%Y-%m-%d'
             ).date()
 
+            meses = {
+                1: 'ENERO',
+                2: 'FEBRERO',
+                3: 'MARZO',
+                4: 'ABRIL',
+                5: 'MAYO',
+                6: 'JUNIO',
+                7: 'JULIO',
+                8: 'AGOSTO',
+                9: 'SEPTIEMBRE',
+                10: 'OCTUBRE',
+                11: 'NOVIEMBRE',
+                12: 'DICIEMBRE'
+            }
+
+            mes = meses[fecha_objeto.month]
+
             gestor_tiempos.agregar_competencia(
                 fecha=fecha_objeto,
+                mes=mes,
                 lugar=lugar,
                 organiza=organiza,
                 nombre=nombre,
